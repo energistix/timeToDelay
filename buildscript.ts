506584f02d9916e010adf8eb8bc7f89c7240f512
energistix <50068022+energistix@users.noreply.github.com>
2025-01-06 21:44:27 +0100

    clear temporary index.js file after build

--- buildscript.ts
+++ buildscript.ts
@@ -10,8 +10,13 @@ if (version === undefined || version === null) {
   throw new Error("Version not found")
 }
 const bump = process.argv.includes("--bump")
-const newVersionTag = `@version${version[1]}1.${Number(version[2]) + (bump ? 1 : 0)}`
+const newVersionTag = `@version${version[1]}1.${
+  Number(version[2]) + (bump ? 1 : 0)
+}`
 
-let newScript = templateScript.replace("// CODE HERE", code).replace(versionRegex, newVersionTag)
+let newScript = templateScript
+  .replace("// CODE HERE", code)
+  .replace(versionRegex, newVersionTag)
 
-await fs.writeFile("./script.user.js", newScript)
+fs.writeFile("./script.user.js", newScript)
+fs.unlink("./index.js")
